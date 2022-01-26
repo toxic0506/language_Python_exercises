1046797d3917a65946e2ba0fcf5a8e39d560b7d6
Albyvan04 <45383982+Albyvan04@users.noreply.github.com>
2022-01-26 12:45:20 +0100

    Add files via upload

--- array/es_3/es3.py
+++ array/es_3/es3.py
@@ -1,7 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Chiedere in input 10 interi e inserirli in un array.
+#Stampare a video prima tutti i numeri pari e 
+#in seguito tutti i numeri dispari
+
 ar = []
 n = 0
 while n < 10:
-    ar.append(raw_input('Inserisci un numero: '))
+    ar.append(input('Inserisci un numero: '))
     n = n+1
 
 for num in ar:
