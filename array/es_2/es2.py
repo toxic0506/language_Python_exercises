353d2e4791f02b1c350ec8d321c89fd5e721bf0b
Albyvan04 <45383982+Albyvan04@users.noreply.github.com>
2022-01-26 12:44:53 +0100

    Add files via upload

--- array/es_2/es2.py
+++ array/es_2/es2.py
@@ -1,7 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+
+#Chiedere in input 5 interi e inserirli in un array.
+#Stampare a video l'array al contrario.
+
 ar = []
 n = 0
 while n < 5:
-    x = raw_input('Inserisci il numero: ')
+    x = input('Inserisci il numero: ')
     ar.append(x)
     n = n+1
 print(ar[::-1])
