cd3bef7f623fca157ed902adf8cfe1ddea62cff1
Cristiano <cristiano.gregnanin@gmail.com>
2022-02-16 11:58:04 +0100

    Update server.py

--- socket/es_9/server.py
+++ socket/es_9/server.py
@@ -1,25 +1,19 @@
-
 import os, socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(("", 50000))
-s.listen(1)
+s.listen(10)
 conn, addr = s.accept()
 
+fd = os.open("f2.jpg", os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0777)
 
-fd = os.open("f2.jpg", os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0600)
-
-buf=conn.recv(4096)
-os.write(fd,buf)
-
-while len (buf)>0:
-   
-    
+while 1:
+  
     buf = conn.recv(4096)
+    if not buf: break
     os.write(fd,buf)
 
     
 os.close(fd)
-
 conn.close()
