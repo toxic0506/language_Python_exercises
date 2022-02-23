846018b57720c36ff924170405c00d070bbb5da7
Cristiano <cristiano.gregnanin@gmail.com>
2022-02-23 10:16:35 +0100

    Create server_concorrente.py

---
+++ socket/es_10/server/server_concorrente.py
@@ -0,0 +1,37 @@
+import os
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(("", 50000))
+s.listen(10)
+
+while True:
+    conn, addr = s.accept()
+    pid = os.fork()
+    if pid==0:
+    
+      filename = conn.recv(4096).decode()
+
+      exist = os.path.isfile("./" + filename)
+      conn.send(str(exist).encode())
+
+      if(exist):
+          print("Nome file giá presente")
+          conn.close()
+          continue
+
+      fd = os.open(filename, os.O_WRONLY | os.O_CREAT | os.O_EXCL, 0o777)
+
+      print("Ricezione di %s" % filename)
+
+      while True:
+          buf = conn.recv(4096)
+          if not buf:
+              break
+          os.write(fd, buf)
+
+      print("Ricevuto")
+      conn.close()
+      os.close(fd)
+      os._exit()
