3be9375c49c1f5cd93df6d379b79e58def6fe200
AliceFontanesi <94352195+AliceFontanesi@users.noreply.github.com>
2023-11-22 10:11:10 +0100

    modifica es1.py

--- funzioni/es_1/es1.py
+++ funzioni/es_1/es1.py
@@ -6,8 +6,12 @@
 def somma(n1, n2):
     return n1 + n2
 
-
-n1 = int(input("Inserire un primo numero: "))
-n2 = int(input("Inserire un secondo numero: "))
+while True:
+    n1 = int(input("Inserire il primo numero (maggiore o uguale a 0): "))
+    n2 = int(input("Inserire il secondo numero (maggiore o uguale a 0): "))
+    if n1 > 0 and n2 > 0:
+        break
+    else:
+        print("Inserire numeri maggiori o uguali a 0")
 
 print("%d + %d = %d" % (n1, n2, somma(n1, n2)))
